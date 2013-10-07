b7143b0650a13c1c25ebf7999bcb0ec5f63666dd
App Academy Student <student@appacademy.io>
2013-10-06 20:18:42 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Autoformbot::Application.config.secret_token = '24e4c9d202fe56da36498eff8c0ca58b764de891f39a0e56edd5d7b85e28edc8dc04d2d477c924fa4cea20d82eeecc4caeeb52dec9e119f2ad649f24108432cf'
