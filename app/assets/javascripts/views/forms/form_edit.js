cde451b6da747aaa82b1fb0b95f9865b090668d1
Rusty Toms <jonathantoms5@gmail.com>
2013-10-08 11:31:55 -0400

    adds events to formEdit view to make form fields appear alternately editable or fixed

--- app/assets/javascripts/views/forms/form_edit.js
+++ app/assets/javascripts/views/forms/form_edit.js
@@ -1,11 +1,53 @@
 AFB.Views.FormEdit = Backbone.View.extend({
+  events: {
+    "mouseenter .field" : "onHover",
+    "mouseleave .field" : "onBlur",
+    "click .field" : "focused",
+    "focusout .field" : "unfocused"
+  },
+
   initialize: function(){
-    console.log('FormEdit View initialized')
+    console.log('FormEdit View initialized');
   },
 
   render: function(){
     this.$el.empty();
+    this.$el.html(JST["forms/edit_form"]);
 
     return this;
+  },
+
+  onHover: function (event) {
+    console.log(event.target);
+    console.log("onHover");
+    $(event.target).removeClass("reset");
+  },
+
+  onBlur: function(event) {
+    console.log(event.target);
+    console.log("onBlur");
+    if(!$(event.target).hasClass("focused")) {
+      $(event.target).addClass("reset");
+    }
+  },
+
+  focused: function (event) {
+    console.log(event.target);
+    console.log("focused");
+
+    $(event.target).removeClass("reset");
+    $(event.target).addClass("focused");
+  },
+
+  unfocused: function(event){
+    console.log(event.target);
+    console.log("unfocused");
+
+    $(event.target).addClass("reset");
+    $(event.target).removeClass("focused");
+  },
+
+  cleanUp: function(){
+    this.undelegateEvents();
   }
 })
