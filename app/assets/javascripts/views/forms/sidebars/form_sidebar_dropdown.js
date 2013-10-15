5264884d1ae4108943996bc227ba89d9912ff63e
Rusty Toms <jonathantoms5@gmail.com>
2013-10-14 21:10:06 -0400

    adds dropdown box and some dropdown customization, working on adding number of options

--- app/assets/javascripts/views/forms/sidebars/form_sidebar_dropdown.js
+++ app/assets/javascripts/views/forms/sidebars/form_sidebar_dropdown.js
@@ -4,16 +4,52 @@ AFB.Views.FormSidebarDropdown = Backbone.View.extend({
 
   addField: function(){
     var form = this.model.get('form_text');
-    $form = $(form).append(this.field);
-    form = $('<div>').append($form.clone()).html();
-    this.model.set('form_text', form);
+    var $form = $(form).append(this.field);
+    this.model.set('form_text', $form.prop('outerHTML'))
   },
+
 
   render: function(){
     console.log("rendering FormSidebarDropdown");
     this.$el.html(JST['forms/sidebars/dropdown_options']({
-      field: this.options.field
+      field: (this.options.field || this.field)
     }));
+
     return this;
+  },
+
+  updateValues: function(event){
+    console.log("in FormSidebarDropdown#updateValues");
+    $form = $(this.model.get('form_text'))
+    $target = $form.find('.editing')
+
+    if (event.target.name === 'dropdown') {
+
+      var $options = $(this.field).find('.dropdowns')
+      $options.empty();
+      var numOptions = event.target.value;
+
+      for(var i=0; i<numOptions; i++){
+        var name = "dropdownOption" + i;
+        $option = $target.find('.' + name)
+
+        if ($option.length === 0){
+          var option = "<option class=" + name + "></option>"
+          $target.find('select').append(option);
+        }
+
+        var optionOptions = JST['forms/sidebars/dropdown_option']({
+          i: i,
+          name: name,
+          '$field': $options
+        })
+
+       $options.append(optionOptions);
+
+       this.model.set('form_text', $form.prop('outerHTML'));
+      }
+    } else {
+      AFB.Views.FormMaster.updateValues(event, this.model);
+    }
   }
 })
