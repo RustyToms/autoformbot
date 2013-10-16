6c031a0bac4129f1ec717c4555a8a365c730b648
Rusty Toms <jonathantoms5@gmail.com>
2013-10-16 15:24:41 -0400

    adds telephone number sidebar

--- app/assets/javascripts/views/forms/sidebars/form_sidebar_telnum.js
+++ app/assets/javascripts/views/forms/sidebars/form_sidebar_telnum.js
@@ -7,15 +7,21 @@ AFB.Views.FormSidebarTelnum = Backbone.View.extend({
 
   render: function(){
     console.log("rendering FormSidebarTelnum");
-    this.$el.html("<div class='sidebar'><h2>Nothing here yet</h2></div>")
+    this.$el.html(JST['forms/sidebars/telnum_options']({
+      field: (this.options.field || this.field)
+    }));
     return this;
   },
 
   updateValues: function(event){
-    console.log("in FormSidebarTextarea#updateValues");
+    console.log("in FormSidebarTelnum#updateValues");
     if ($(event.target).attr('name')=== 'telnum-label' ){
       var name = 'results[' + event.target.value + ']'
       this.model.updateAttribute('.editing .textbox', 'name', name);
+    } else if($(event.target).data('attribute') === 'maxlength' ){
+      console.log('adjusting field size');
+      var size = parseInt(event.target.value) + 3;
+      this.model.updateAttribute('.editing .telnum', 'size', size);
     }
     this.model.updateValues(event);
   }
