81f5a842cf114a2ab36b5b30db1821ee63e225ee
Jonathan Rusty Toms <jonathantoms5@gmail.com>
2013-10-20 19:19:30 -0400

    forms seem to be submitting okay, with an optional url redirect on success

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -8,16 +8,13 @@ class ResultsController < ApplicationController
     @result = Result.new
     @result.form_id = params[:form_id]
     @result.result = params[:results]
-    sleep 3
+    @params = params
     if @result.save
-      fail
-      flash[:notice] = "Form submitted!"
-      render json: @result      # 
-      # redirect_to 'google.com'
-    else      # 
-      # fail
-      # flash.now[:errors] = @result.errors.full_messages
-      render json: @result, status: :unprocessable_entity
+      # render json: @result      
+      render text: ""
+    else  
+      render json: @params
+      # render json: @result, status: :unprocessable_entity, @result.errors.full_messages
     end
   end
   
