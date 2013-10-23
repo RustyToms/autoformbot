ddeaf5a373b3104ff7f4115678b916d13015e3b3
App Academy Student <student@appacademy.io>
2013-10-23 18:05:10 -0400

    more css for notices and color, about to rearrange how a user and account gets created

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,11 @@
 class StaticPagesController < ApplicationController
 
   def home
+    if current_user
+      @url = current_user.account.url_name
+    else
+      @url = ''
+    end
     render :home
   end
 end
