8695c5b333278fb1b4ebddb586ab37aac39dc97d
Rusty Toms <jonathantoms5@gmail.com>
2013-11-08 13:34:26 -0500

    textbox updated

--- app/assets/javascripts/views/forms/sidebars/form_sidebar_textbox.js
+++ app/assets/javascripts/views/forms/sidebars/form_sidebar_textbox.js
@@ -8,7 +8,7 @@ AFB.Views.FormSidebarTextbox = Backbone.View.extend({
   render: function(){
     console.log("rendering FormSidebarTextbox");
     this.$el.html(JST['forms/sidebars/textbox_options']({
-      field: (this.field || this.seed)
+      $field: $(this.field || this.seed)
     }));
 
     return this;
@@ -17,7 +17,7 @@ AFB.Views.FormSidebarTextbox = Backbone.View.extend({
   updateValues: function(event){
     console.log("in FormSidebarTextbox#updateValues");
     if ($(event.target).attr('name') === 'textbox-label' ){
-      var name = 'results[' + event.target.value + ']';
+      var name = event.target.value;
       this.model.updateAttribute('.editing .textbox', 'name', name);
     }
     this.model.updateValues(event);
